d9d43862fa8412e55999d63d3a83bc02019a9545
Srishruti24 <sri.shruti24@gmail.com>
2025-09-22 13:48:52 +0530

    header-Change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/header";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import ServiceSection from "./components/ServiceSection";
