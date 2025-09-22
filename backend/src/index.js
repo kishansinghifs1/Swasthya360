ecf9a5c51ed7549223181e265ef8a7341b856b47
kishansinghifs1 <kishansingh956196@gmai.com>
2025-09-22 17:30:56 +0530

    Update CORS configuration to allow requests from localhost:5173

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500", 
+    origin: "localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true 
 }));
